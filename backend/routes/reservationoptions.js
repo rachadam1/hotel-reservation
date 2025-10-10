3fcf74f2072c750e5ef58dd1a51661b2f817c244
rachid <rachadam1938@email.com>
2025-10-10 13:32:23 +0300

    Sauvegarde de tous les fichiers modifiés

--- backend/routes/reservationoptions.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const reservationOptionsController = require('../controllers/reservationOptionsController');
-
-router.post('/', reservationOptionsController.addOptionToReservation);
-router.get('/:reservation_id', reservationOptionsController.getOptionsForReservation);
-
-module.exports = router;
